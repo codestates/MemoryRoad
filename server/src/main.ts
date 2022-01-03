300ab034db033b7562657706461802a43db17a6f
PippasSong <tlmag2108@gmail.com>
2022-01-03 15:54:08 +0900

    feat : add swagger, add GET routes
    - 스웨거 전역 미들웨어로 추가, 사용자의 루트 조회기능 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { swaggerUi, swaggerSpec } from './swaggerDoc';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //스웨거 적용을 위한 전역 미들웨어
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   await app.listen(80);
 }
 bootstrap();
