7700b0e7e744e2b3d54892e72dce7f7a3fb0bc98
PippasSong <tlmag2108@gmail.com>
2022-01-04 22:18:47 +0900

    fix : modify controller response
    - 응답 객체를 잘못 작성해서 응답이 가지 않은 현상 해결

--- server/src/routes/routes.controller.ts
+++ server/src/routes/routes.controller.ts
@@ -47,10 +47,10 @@ export class RoutesController {
       });
     }
 
-    return {
+    return res.status(201).json({
       code: 201,
       message: 'created',
-    };
+    });
   }
 
   //path 파라미터를 받는다.
@@ -71,10 +71,10 @@ export class RoutesController {
         });
       }
 
-      return {
+      return res.status(200).json({
         code: 200,
         message: 'updated',
-      };
+      });
     } catch (err) {
       console.log(err);
       return res.status(500).json({
@@ -84,11 +84,24 @@ export class RoutesController {
     }
   }
 
+  //TODO: 해당 사용자가 작성한 루트만 삭제할 수 있어야 한다.
+  // 루트를 삭제하면 루트를 참조하는 핀, 핀을 참조하는 사진들이 전부 삭제된다.(외래키에 on delete : cascade 속성이 있다.)
   @Delete('/:routeId')
   async deleteRoute(@Param('routeId') routeId: number, @Res() res: Response) {
     try {
       const deleteResult = await this.routesService.deleteRoute(routeId);
-      return deleteResult;
+      if (!deleteResult.affected) {
+        //없는 루트, 또는 다른 유저가 작성한 루트를 삭제 하려는 경우
+        return res.status(404).json({
+          code: 404,
+          message: 'not found',
+        });
+      }
+
+      return res.status(200).json({
+        code: 200,
+        message: 'deleted',
+      });
     } catch (err) {
       console.log(err);
       return res.status(500).json({
