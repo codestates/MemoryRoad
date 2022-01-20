4e2caf4df17f60c8a38d794522d7973c9165691b
jyang510 <yayww@naver.com>
2022-01-21 01:22:53 +0900

    진행중

--- client/src/components/colorSelectBox/colorSelectBoxForMap.tsx
+++ client/src/components/colorSelectBox/colorSelectBoxForMap.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../redux/reducer';
 import { Route1 } from '../../types/searchRoutesTypes';
 import './colorSelectBox.css';
 
@@ -7,18 +9,6 @@ type Props = {
   setAllRoutes: React.Dispatch<React.SetStateAction<Array<Route1>>>;
   findAllRoute: any;
 };
-
-const colorsName = [
-  'red',
-  'orange',
-  'yellow',
-  'yellowGreen',
-  'green',
-  'sky',
-  'blue',
-  'purple',
-  'pink',
-];
 
 function ColorSelectBox({ setColorIdx, setAllRoutes, findAllRoute }: Props) {
   const [clickedColorSelect, setClickedColorSelect] = useState(false);
@@ -29,6 +19,15 @@ function ColorSelectBox({ setColorIdx, setAllRoutes, findAllRoute }: Props) {
   const selectColor = (event: any) => {
     setSelectedCorlorId(event.target.id);
   };
+  const dispatch = useDispatch();
+  /* redux 전역 상태관리 */ // 왜 type 할당 : RootState는 되고 RootPersistState는 안되나요 ?
+
+  const colorUrls: any = useSelector(
+    (state: RootState) => state.createRouteReducer.whiteColorUrl,
+  );
+  const colorsName: any = useSelector(
+    (state: RootState) => state.createRouteReducer.colorName,
+  );
 
   //조건에 맞는 루트 배열을 state값에 저장한다.
   async function getAllRoute(colorIdx: number) {
@@ -46,17 +45,6 @@ function ColorSelectBox({ setColorIdx, setAllRoutes, findAllRoute }: Props) {
     }
   }
 
-  const colors = [
-    'http://127.0.0.1:5500/client/public/img/red_dot.png',
-    'http://127.0.0.1:5500/client/public/img/orange_dot.png',
-    'http://127.0.0.1:5500/client/public/img/yellow_dot.png',
-    'http://127.0.0.1:5500/client/public/img/yellowGreen_dot.png',
-    'http://127.0.0.1:5500/client/public/img/green_dot.png',
-    'http://127.0.0.1:5500/client/public/img/sky_dot.png',
-    'http://127.0.0.1:5500/client/public/img/blue_dot.png',
-    'http://127.0.0.1:5500/client/public/img/purple_dot.png',
-    'http://127.0.0.1:5500/client/public/img/pink_dot.png',
-  ];
   return (
     <>
       <div
@@ -68,11 +56,11 @@ function ColorSelectBox({ setColorIdx, setAllRoutes, findAllRoute }: Props) {
           <img
             alt="selected-dot"
             className="selectbox-color-selected-option"
-            src={colors[Number(selectedColorId)]}
+            src={colorUrls[Number(selectedColorId)]}
           />
         </button>
         <ul className="selectbox-color-optionList">
-          {colors.map((color, idx) => {
+          {colorUrls.map((color: any, idx: any) => {
             const strArr = color.split('/');
             return (
               <li
